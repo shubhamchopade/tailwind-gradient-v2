5482e2c1a6dbe24dca5db0e01fe6839095ac59c3
Shubham Chopade <31898834+shubhamchopade@users.noreply.github.com>
2021-04-25 03:50:45 +0530

    naming random colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css';
-import { ColorPalette } from './components/ColorPalette';
 import { GetTailwindConfigFile } from './components/GetTailwindConfigFile';
 
 function App() {
   return (
     <div className="App">
       <GetTailwindConfigFile />
-        <ColorPalette />
     </div>
   );
 }
--- src/components/ColorPalette.tsx
+++ src/components/ColorPalette.tsx
@@ -6,12 +6,12 @@ import { colors } from "../data/colors";
 export const ColorPalette = () => {
   const hsla = useRandom();
   const { hue, saturation, lightness, alpha } = hsla;
-  console.log(hue, saturation, lightness, alpha);
+
 
   return (
     <div className="flex">
-      {colors.map((color) => (
-        <ColorBall color={color[400]} />
+      {colors.map((color, index) => (
+        <ColorBall key={index} color={color[400]} />
       ))}
     </div>
   );
