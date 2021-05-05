b8fcb7e38924b3eb9938c03d8c5e16be6a373bb9
Shubham Chopade <31898834+shubhamchopade@users.noreply.github.com>
2021-05-05 23:37:31 +0530

    copied overlay

---
+++ src/components/common/OverlayCopiedToClipboard.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface Props {}
+
+const OverlayCopiedToClipboard: React.FunctionComponent<Props> = () => {
+  return (
+    <div>
+      <div className="w-full h-56 sm:h-72 backdrop-filter backdrop-blur-sm bg-white bg-opacity-50 absolute top-0 left-0 z-10 rounded-md">
+        <div className="grid place-content-center text-center my-auto h-full text-gray-900">
+          <p>COPIED COLOR HEX CODE</p>
+          <h2 className="text-3xl sm:text-5xl font-black ">#8BAFAC</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OverlayCopiedToClipboard;
